10fac5a1195a10ee6a1b31b317354afd034c446b
Tenuxi <jaaltonen86@gmail.com>
2019-08-20 14:37:31 +0300

    react java 6 tehty

--- react/javascrip_harjoitukset/6/koodi.js
+++ react/javascrip_harjoitukset/6/koodi.js
@@ -1,1 +1,14 @@
 console.log ("JS ladattu");
+
+
+let div1 = document.getElementById("div1");
+let div2 = document.getElementById("div2");
+
+paivitaKoko = () => {
+    console.log("Div2 Leveys:", div2.offsetWidth, "px");
+    console.log("Div1 korkeus:", div1.offsetHeight, "px");
+    div1.style.height = div2.offsetWidth + "px";
+}
+
+window.addEventListener("resize", paivitaKoko);
+
