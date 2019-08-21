421a1a96761313604c641fecd0aecd88a8329728
Tenuxi <jaaltonen86@gmail.com>
2019-08-21 12:33:06 +0300

    Lisää php harjoituksia

---
+++ symphony/php/harjoitus5/index.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- Fontit -->
+    <link href="https://fonts.googleapis.com/css?family=Dosis&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Sans&display=swap" rel="stylesheet">
+
+    <!-- Tyylit -->
+        <!-- Bootsrtap -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <!-- .css -->
+        <!-- <link rel="stylesheet" href="style.css"> --> <!--Tumma teema-->
+        <link rel="stylesheet" href="style_vaalea.css"> <!--Vaalea teema-->
+        
+
+    <!-- Title ------------------------------------------------------------------>
+    <title>Harjoitus 5</title>
+
+
+</head>
+<body>
+
+
+
+<!-- Poista ulos kommentoinnit diveistä joita haluat käyttää! -------------------->
+
+
+    <!-- Header ------------------------------------------------------------------>
+    <header>Harjoitus 5</header>
+
+
+
+    <!-- Navigaatio -------------------------------------------------------------->
+
+<!--
+    <ul id="navigaatio">
+        <li class="nav-item">
+            <a class="nav-link active" href="#">Linkki 1</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Linkki 2</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Linkki 3</a>
+        </li>
+    </ul>
+-->
+
+
+    <!-- Nappit ------------------------------------------------------------------>
+
+<!--
+    <div id="napit">
+        <button type="button" class="btn btn-primary">Nappi</button>
+        <button type="button" class="btn btn-primary">Nappi</button>
+    </div>
+    -->
+
+
+
+    <!-- Pieni "Huom" Div -------------------------------------------------------->
+
+<!--
+    <div id="huomautus" class="alert alert-danger" role="alert">
+        
+    </div>
+-->
+
+
+
+    <!-- 95% Leveä Div ------------------------------------------------------------>
+
+
+    <div id="leveaDiv" class="container-fluid">
+        
+<pre>
+Koodaa Linkki-luokka, johon voi tallettaa WWW-sivun tiedot (ominaisuudet)
+
+* id
+
+* linkki
+
+* otsikkokuvaus
+
+* avainsana
+
+Lisää luokkaan konstruktori tietojen tallentamista varten. Lisää luokkaan TulostaTiedot-metodi,
+jonka avulla linkin tiedot voidaan tulostaa, niin että kukin ominaisuus on omalla rivillä yhdessä ominaisuuden kanssa.
+
+Koodaa sovellus (linkkiTesti.php) jolla voit testata luokan toimintaa. Käytä testauksessa seuraavia tietoja
+
+* id = 1
+
+* linkki = ”http://www.mysql.com”
+
+* otsikko = ”MySQL-tietokannan kotisivu”
+
+* kuvaus = ”Relaatiotietokanta”
+
+* avainsana = ”mysql”
+</pre>
+
+    </div>
+
+
+
+
+    <!-- 70% Leveä Div ------------------------------------------------------------>
+
+<!--
+    <div id="kapeaDiv" class="container">
+
+        
+
+    </div>
+-->
+
+
+
+    <!-- Footer ------------------------------------------------------------------->
+    <footer class="footer mt-auto py-3">
+        <div class="container">
+            <span class="text-muted">Template © Joonas - 2019</span>
+        </div>
+    </footer>
+
+
+<!-- JS yms. -->
+
+
+<!--
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+-->
+
+
+</body>
+</html>
---
+++ symphony/php/harjoitus8/index.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- Fontit -->
+    <link href="https://fonts.googleapis.com/css?family=Dosis&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Sans&display=swap" rel="stylesheet">
+
+    <!-- Tyylit -->
+        <!-- Bootsrtap -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <!-- .css -->
+        <link rel="stylesheet" href="style.css"> <!--Tumma teema-->
+        <!-- <link rel="stylesheet" href="style_vaalea.css"> --> <!--Vaalea teema-->
+        
+
+    <!-- Title ------------------------------------------------------------------>
+    <title>Harjoitus 8</title>
+
+
+</head>
+<body>
+
+
+
+<!-- Poista ulos kommentoinnit diveistä joita haluat käyttää! -------------------->
+
+
+    <!-- Header ------------------------------------------------------------------>
+    <header>Harjoitus 8</header>
+
+
+
+    <!-- Navigaatio -------------------------------------------------------------->
+
+<!--
+    <ul id="navigaatio">
+        <li class="nav-item">
+            <a class="nav-link active" href="#">Linkki 1</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Linkki 2</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Linkki 3</a>
+        </li>
+    </ul>
+-->
+
+
+    <!-- Nappit ------------------------------------------------------------------>
+
+<!--
+    <div id="napit">
+        <button type="button" class="btn btn-primary">Nappi</button>
+        <button type="button" class="btn btn-primary">Nappi</button>
+    </div>
+    -->
+
+
+
+    <!-- Pieni "Huom" Div -------------------------------------------------------->
+
+<!--
+    <div id="huomautus" class="alert alert-danger" role="alert">
+        
+    </div>
+-->
+
+
+
+    <!-- 95% Leveä Div ------------------------------------------------------------>
+
+
+    <div id="leveaDiv" class="container-fluid">
+        
+<pre>
+Eräässä pelissä on PlayerHealthManager niminen luokka, jossa on seuraavat yksityiset ominaisuudet
+
+* currentHP // pelihahmon nykyinen terveys
+
+* maxHP // pelihahmon maksimiterveys
+
+Lisää luokkaan seuraavat metodit. Tässä harjoituksessa ei tarvitse lisä get- ja set-metodeja.
+
+* addPlayerHealth($healthAmmount)
+
+* setMaxHealth
+
+* hurtPlayer($damageToGive)
+
+Koodaa hurtPayerHealth-metodi, joka vähentää pelihahmon terveyttä (damageToGive).
+
+Koodaa setMaxHealth, joka asettaa pelihahmon maksimiterveyden.
+
+Koodaa addPlayerHealth-metodi, joka lisää pelihahmon terveyttä (healthAmmount). Koodaa metodiin seuraava tarkistus. Mitä tarkistus tekee?
+
+* jos (nykyinen HP > maksimi HP) niin
+
+o aseta maksimi HP
+</pre>
+
+    </div>
+
+
+
+
+    <!-- 70% Leveä Div ------------------------------------------------------------>
+
+<!--
+    <div id="kapeaDiv" class="container">
+
+        
+
+    </div>
+-->
+
+
+
+    <!-- Footer ------------------------------------------------------------------->
+    <footer class="footer mt-auto py-3">
+        <div class="container">
+            <span class="text-muted">Template © Joonas - 2019</span>
+        </div>
+    </footer>
+
+
+<!-- JS yms. -->
+
+
+<!--
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+-->
+
+
+</body>
+</html>
